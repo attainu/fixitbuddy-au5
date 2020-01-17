d42392d422f2acbfe52816ac2f9b6ffe5c064d1c
kshitij-singh-au5 <56304196+kshitij-singh-au5@users.noreply.github.com>
2020-01-17 12:29:41 +0530

    mvc done

--- app.js
+++ app.js
@@ -5,263 +5,91 @@ var multiparty = require("multiparty")
 const mongodb = require("mongodb")
 var session = require("express-session")
 var cloudinary = require("cloudinary")
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose")
 
 var app = express()
 
 app.use(express.static("public"))
 app.use(express.static("uploads"))
 
-app.set("view engine","hbs")
-app.use(bodyParser.urlencoded({extended:false}))
+app.set("view engine", "hbs")
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
     secret: "qwerty",
-    cookie:{
-        maxAge: 1000*100,
+    cookie: {
+        maxAge: 1000 * 100 * 60,
         httpOnly: true,
         path: "/"
     }
 
-})
-)
+}))
 
-var url = "mongodb://localhost:27017"
+var url = "mongodb+srv://kshitij7:12345@cluster0-6awrx.mongodb.net/fixitbuddy?retryWrites=true&w=majority"
 var DB = ""
-var dbName= "fixit-buddy"
 
-mongodb.MongoClient.connect(url,function(err,client){
-    if(err){
-        console.log(err)
+mongoose.connect(
+    url,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
     }
-    else{
-        DB = client.db(dbName)
-    }
-})
+  );
 
-cloudinary.config({
-    cloud_name: "blues1905",
-    api_key: "642721642791148",
-    api_secret: "u-yjKxbeck9WFW7Z4QJeegx77Io"
-})
+// mongodb.MongoClient.connect(url, function (err, client) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("Atlas Connected")
+//         DB = client.db("fixitbuddy");
+//     }
+// })
 
-app.get("/",function (req,res) {
-    DB.collection("services").find({}).toArray(function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            var cleaning = result.filter(function (elem) {
-                return elem.service=="cleaning"
-            })
-            var electricianPlumbing = result.filter(function (elem) {
-                return elem.service=="electrician-plumbing"
-            })
-            var appliancesRepair = result.filter(function (elem) {
-                return elem.service=="appliances-repair"
-            })
-            var fitnessYoga = result.filter(function (elem) {
-                return elem.service=="fitness-yoga"
-            })
+// cloudinary.config({
+//     cloud_name: "blues1905",
+//     api_key: "642721642791148",
+//     api_secret: "u-yjKxbeck9WFW7Z4QJeegx77Io"
+// })
 
-            res.render("home",{
-                cleaning: cleaning,
-                electricianPlumbing: electricianPlumbing,
-                appliancesRepair: appliancesRepair,
-                fitnessYoga: fitnessYoga
-            })
-        }
-    })
-    
-})
-
-app.get("/bathroom-cleaning",function (req,res) {
-    DB.collection("services").findOne({name:"Bathroom Deep Cleaning"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-        
-    })
-})
-
-app.get("/carpet-cleaning",function (req,res) {
-    DB.collection("services").findOne({name:"Carpet Cleaning"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/home-cleaning",function (req,res) {
-    DB.collection("services").findOne({name:"Full Home Deep Cleaning"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/kitchen-cleaning",function (req,res) {
-    DB.collection("services").findOne({name:"Kitchen Deep Cleaning"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/electricians",function (req,res) {
-    DB.collection("services").findOne({name:"Electricians"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/plumbers",function (req,res) {
-    DB.collection("services").findOne({name:"Plumbers"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/carpenters",function (req,res) {
-    DB.collection("services").findOne({name:"Carpenters"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/woodwork",function (req,res) {
-    DB.collection("services").findOne({name:"Woodwork and Furniture Making"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/ac-repair",function (req,res) {
-    DB.collection("services").findOne({name:"AC Service and Repair"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/washing-machine",function (req,res) {
-    DB.collection("services").findOne({name:"Washing Machine Service & Repair"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/refrigerator",function (req,res) {
-    DB.collection("services").findOne({name:"Refrigerator Repair"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/geyser",function (req,res) {
-    DB.collection("services").findOne({name:"Geyser Service and Repair"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/fitness",function (req,res) {
-    DB.collection("services").findOne({name:"Fitness Trainer at Home"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
-
-app.get("/yoga",function (req,res) {
-    DB.collection("services").findOne({name:"Yoga Trainer at Home"},function (err,result) {
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("service",{
-                service: result
-            })
-        }
-    })
-})
+// var email = []
+// console.log(email)
 
 
-app.get("/profile",function(req,res){
-    res.render("profile")
-})
+const service = require("./controller/service.js")
+
+const home = require("./controller/home.js")
+
+const user = require("./controller/user.js")
 
 
+app.get("/", home.Controller.home)
+
+app.get("/service", service.Controller.service)
+
+app.get("/signuplogin", user.Controller.signuplogin )
+
+
+app.post("/usersignup", user.Controller.usersignup)
+
+app.post("/otp", user.Controller.otp)
+
+app.post("/userlogin", user.Controller.userlogin)
+
+app.post("/bookedService", user.Controller.bookedService)
+
+// app.use(user.Controller.middle)
+
+app.get("/profile", user.Controller.profile)
+
+app.post("/updateAddress", user.Controller.updateAddress)
+
+app.post("/updateuserinfo", user.Controller.updateuserinfo)
+
+app.get("/logout", user.Controller.logout)
+
+/////////////////////////////Admin Routes///////////////////////////
 app.get("/Admin", function(req, res){
     res.render("Admin",{
 
@@ -274,10 +102,7 @@ app.get("/user-signin", function(req, res){
     res.render("signup",{
 
     });
-
-app.get('/',function(req,res){
-    res.render('dashboard')
-});
+})
     
 app.get('/dashboard',function(req,res){
     res.render('dashboard')
@@ -294,6 +119,7 @@ app.get('/orderapproval',function(req,res){
 app.get('/information',function(req,res){
     res.render('information')
 });
-    
-});
+
+
+
 app.listen(4000);
